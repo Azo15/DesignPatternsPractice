1d73d01de3a0ee2683b6e80fbc01bdd38305736b
Azo Ismail <ismailazo260@gmail.com>
2026-01-20 16:14:01 +0300

    Add MusicPlayer functionality with state management and example usage

--- StateDesignPatternBut/src/Main.java
+++ StateDesignPatternBut/src/Main.java
@@ -2,8 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
-
+        MusicPlayer musicPlayer = new MusicPlayer();
+        musicPlayer.pressPlay();
+        musicPlayer.pressPause();
+        musicPlayer.pressStop();
     }
 }
